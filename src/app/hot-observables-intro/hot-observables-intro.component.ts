69a07bda083e54c64e2dd70211e77dab9137392b
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2021-01-24 17:21:57 -0300

    Criando Hot Observables

--- src/app/hot-observables-intro/hot-observables-intro.component.ts
+++ src/app/hot-observables-intro/hot-observables-intro.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { fromEvent, Observable } from 'rxjs';
+import { fromEvent, Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-hot-observables-intro',
@@ -19,11 +19,50 @@ export class HotObservablesIntroComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    const myBtnClickObservable: Observable<number> = fromEvent(
-      this.button.nativeElement, 'click'
-    );
-    myBtnClickObservable.subscribe(event => console.log('button clicked 1'));
-    myBtnClickObservable.subscribe(event => console.log('button clicked 2'));
+    // const myBtnClickObservable: Observable<number> = fromEvent(
+    //   this.button.nativeElement, 'click'
+    // );
+    // myBtnClickObservable.subscribe(event => console.log('button clicked 1'));
+    // myBtnClickObservable.subscribe(event => console.log('button clicked 2'));
+
+    class Producer {
+      private myListeners = [];
+      private n = 0;
+      private id;
+
+      addListener(l): void {
+        this.myListeners.push(l);
+        console.log(this.myListeners.length);
+      }
+
+      start(): void {
+        this.id = setInterval(() => {
+          this.n++;
+          console.log('From Producer:', this.n);
+          for (const l of this.myListeners) {
+            l(this.n);
+          }
+        }, 1000);
+      }
+
+      stop(): void {
+        clearInterval(this.id);
+      }
+
+    }
+
+    const producer: Producer = new Producer();
+    producer.start();
+    setTimeout(() => {
+      producer.addListener(n => console.log('From listener 1', n));
+      producer.addListener(n => console.log('From listener 2', n));
+    }, 4000);
+
+    const myHotObservable = new Observable((observer: Observer<number>) => {
+      producer.addListener((n) => observer.next);
+    });
+    myHotObservable.subscribe(n => console.log('From subscribe 1', n));
+    myHotObservable.subscribe(n => console.log('From subscribe 2', n));
   }
 
 }
